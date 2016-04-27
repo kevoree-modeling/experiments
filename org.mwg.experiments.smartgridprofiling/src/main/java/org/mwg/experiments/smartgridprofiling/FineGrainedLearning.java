8972f8cdf7fc1c3ca07428a556cecb37690e33f4
Assaad Moawad <assaad.moawad@uni.lu>
2016-04-27 18:44:33 +0200

    Update first commit

---
+++ org.mwg.experiments.smartgridprofiling/src/main/java/org/mwg/experiments/smartgridprofiling/FineGrainedLearning.java
@@ -0,0 +1,131 @@
+package org.mwg.experiments.smartgridprofiling;
+
+import org.mwg.*;
+import org.mwg.core.NoopScheduler;
+import org.mwg.profiling.GaussianSlotProfiling;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.TimeZone;
+import java.util.TreeMap;
+
+/**
+ * Created by assaad on 27/04/16.
+ */
+public class FineGrainedLearning {
+    final static String csvdir="/Users/assaad/work/github/data/consumption/londonpower/";
+
+    public static void main(String[] arg){
+        final Graph graph = GraphBuilder.builder()
+                .withFactory(new GaussianSlotProfiling.Factory())
+                .withScheduler(new NoopScheduler())
+               // .withOffHeapMemory()
+                .withMemorySize(1_000_000)
+                .withAutoSave(10_000)
+                .withStorage(new LevelDBStorage(csvdir+"leveldb/"))
+                .build();
+        graph.connect(new Callback<Boolean>() {
+            public void on(Boolean result) {
+
+                // Start training: 1348961400000
+                // End training:1380000600000
+                // End testing: 1390001400000
+
+
+                int globaltotal = 0;
+                long starttime = System.nanoTime();
+                try {
+
+                    String line = "";
+                    int nuser=0;
+                    String cvsSplitBy = ",";
+                    int powerValue;
+                    String username;
+                    String[] splitted;
+                    long timestamp;
+
+                    File dir = new File(csvdir + "users/");
+                    File[] directoryListing = dir.listFiles();
+                    if (directoryListing != null) {
+                        for (File file : directoryListing) {
+                            if (file.isDirectory() || file.getName().equals(".DS_Store")) {
+                                continue;
+                            }
+                            BufferedReader br = new BufferedReader(new FileReader(file));
+
+                            username = file.getName().split("\\.")[0];
+                            Node smartmeter= graph.newNode(0,0);
+                            final Node profiler=graph.newNode(0,0,GaussianSlotProfiling.NAME);
+                            profiler.set(GaussianSlotProfiling.SLOTSNUMBER,12); //one slot every hour
+
+                            smartmeter.set("name",username);
+                            smartmeter.add("profile",profiler);
+                            graph.index("nodes",smartmeter,new String[]{"name"},null);
+
+                            while ((line = br.readLine()) != null) {
+                                try {
+
+                                    splitted = line.split(cvsSplitBy);
+                                    if (splitted.length != 2) {
+                                        continue;
+                                    }
+                                    timestamp = Long.parseLong(splitted[0]);
+                                    powerValue = Integer.parseInt(splitted[1]);
+                                    final int pv=powerValue;
+
+                                    smartmeter.jump(timestamp, new Callback<Node>() {
+                                        @Override
+                                        public void on(Node result) {
+
+
+                                            result.set("power",pv);
+
+                                         /*   result.rel("profile", ( profilers) -> {
+                                                ((GaussianSlotProfiling) profilers[0]).learn(new double[]{pv});
+                                                profilers[0].free();
+                                            });*/
+
+                                            result.free();
+                                        }
+                                    });
+
+
+                                    globaltotal++;
+                                } catch (Exception ex) {
+                                    ex.printStackTrace();
+                                }
+                            }
+
+                            smartmeter.free();
+
+                            nuser++;
+                            //System.out.println(nuser);
+                            if(nuser%10==0){
+                                System.out.println(nuser);
+                            }
+                            br.close();
+                            //  System.out.println("File " + file.getName() + " parsed successfully");
+                        }
+                    }
+
+                    long endtime = System.nanoTime();
+                    double restime = (endtime - starttime) / 1000000000;
+                    System.out.println("Loaded " + globaltotal + " power records in " + restime + " s !");
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
+
+
+
+    }
+}
---
+++ org.mwg.experiments.smartgridprofiling/src/main/java/org/mwg/experiments/smartgridprofiling/GaussianProfile.java
@@ -0,0 +1,85 @@
+package org.mwg.experiments.smartgridprofiling;
+
+/**
+ * Created by assaad on 27/04/16.
+ */
+public class GaussianProfile {
+    private int features;
+    private double[] min;
+    private double[] max;
+    private double[] sum;
+    private double[] sumSquares;
+    private int total;
+
+    public int getTotal() {
+        return total;
+    }
+
+    public double[] getSumSquares() {
+        return sumSquares;
+    }
+
+    public double[] getSum() {
+        return sum;
+    }
+
+    public double[] getMax() {
+        return max;
+    }
+
+    public double[] getMin() {
+        return min;
+    }
+
+
+    public void learn(double[] values){
+        if(total==0){
+            features=values.length;
+            min=new double[features];
+            max=new double[features];
+            sum=new double[features];
+            sumSquares=new double[features*(features+1)/2];
+            int count=0;
+            for(int i=0;i<features;i++){
+                min[i]=values[i];
+                max[i]=values[i];
+                sum[i]=values[i];
+                for (int j = i; j < features; j++) {
+                    sumSquares[count] = values[i] * values[j];
+                    count++;
+                }
+            }
+        }
+        else{
+            int count=0;
+            for(int i=0;i<features;i++){
+                if(values[i]<min[i]){
+                    min[i]=values[i];
+                }
+                if(values[i]>max[i]) {
+                    max[i] = values[i];
+                }
+                sum[i]+=values[i];
+                for (int j = i; j < features; j++) {
+                    sumSquares[count] += values[i] * values[j];
+                    count++;
+                }
+            }
+        }
+        total++;
+    }
+
+
+    public double[] getAvg(){
+        double[] avg = new double[features];
+        if(features==0||total==0){
+            return avg;
+        }
+        else {
+            for(int i=0;i<features;i++){
+                avg[i]=sum[i]/total;
+            }
+        }
+        return avg;
+    }
+}
